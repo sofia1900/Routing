4e7b524d374ce676112db7a6bfe9675ee4227eb8
sofia <sofia.jimlap@educa.jcyl.es>
2024-01-16 19:23:28 +0100

    routing0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./paginas/general/home/home.component";
+import {ContactComponent} from "./paginas/general/contact/contact.component";
+import {AboutComponent} from "./paginas/general/about/about.component";
+import {SigninComponent} from "./paginas/general/signin/signin.component";
+import {NotFoundComponent} from "./paginas/general/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component : HomeComponent},
+  {path: 'contact', component : ContactComponent},
+  {path: 'about', component : AboutComponent},
+  {path: 'signin', component : SigninComponent},
+  {path: '**', component : NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './paginas/general/home/home.component';
+import { ContactComponent } from './paginas/general/contact/contact.component';
+import { AboutComponent } from './paginas/general/about/about.component';
+import { SigninComponent } from './paginas/general/signin/signin.component';
+import { NotFoundComponent } from './paginas/general/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    SigninComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
