14733277fa0bc5cac2312ceb9859d87af9d898eb
sofia <sofia.jimlap@educa.jcyl.es>
2024-01-16 19:34:24 +0100

    routing2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UsuariosComponent} from "./componentes/usuarios/usuarios.component";
+import {PostComponent} from "./componentes/post/post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UsuariosComponent},
+  {path: 'posts', component : PostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
+import { PostComponent } from './componentes/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
